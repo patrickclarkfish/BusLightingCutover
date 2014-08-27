a4bbf16d5ef681d378e3abe6edffc30552482a7d
Patrick Clark <patrickclarkfish@gmail.com>
2014-08-27 14:13:19 -0500

    initial commit

---
+++ BusLightingCutover/BusLightingCutover.cpp
@@ -0,0 +1,17 @@
+/*
+ * BusLightingCutover.cpp
+ *
+ * Created: 8/27/2014 2:12:08 PM
+ *  Author: Patrick
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
